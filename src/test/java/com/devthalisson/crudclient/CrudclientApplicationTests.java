0e460bbc094c6bd54ed9629b533c77a5fe219260
Thalisson <thalissonpop91@gmail.com>
2023-12-03 15:12:55 -0300

    Initial commit

---
+++ src/test/java/com/devthalisson/crudclient/CrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devthalisson.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
