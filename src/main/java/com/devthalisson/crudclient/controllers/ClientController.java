fd7899cf26e83166e7ba64e11643d4931adce9bb
Thalisson <thalissonpop91@gmail.com>
2023-12-03 15:25:57 -0300

    findByID in controller

--- src/main/java/com/devthalisson/crudclient/controllers/ClientController.java
+++ src/main/java/com/devthalisson/crudclient/controllers/ClientController.java
@@ -1,4 +1,24 @@
 package com.devthalisson.crudclient.controllers;
 
+import com.devthalisson.crudclient.entities.Client;
+import com.devthalisson.crudclient.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
 public class ClientController {
+
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> findById(@PathVariable Long id){
+        Client result = service.findById(id);
+        return ResponseEntity.status(200).body(result);
+    }
 }
